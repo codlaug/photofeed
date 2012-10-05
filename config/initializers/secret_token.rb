69975837197f0486b15a24a5c623d0299fd7b500
Scott Schlosser <SSchlosser@wetpaint.com>
2012-10-05 09:24:57 -0700

    Initial commit of photofeed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photofeed::Application.config.secret_token = '2c9e6349e13c40b8af7276584c2341d2fcd66b273ca38c10d980bc774010a668d231aad99866d92f2ca69c302522cc1636d3cd94b4498db9c5e949f72c2b9f01'
