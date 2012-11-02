7a970e8b221b55a6e875d4b7c51914d864dc42f9
Scott Schlosser <scottschlosser@gmail.com>
2012-11-02 14:15:15 -0700

    Moving assets to use asset pipeline and eliminatign duplication of html,head and body tags. Fixing missing div tag in app/views/feed/index.html.erb.

--- config/routes.rb
+++ config/routes.rb
@@ -62,6 +62,6 @@ Photofeed::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
 
-match '/feed' => "feeds#index"
+match '/feed' => "feed#index"
 
 end
