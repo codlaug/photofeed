543a24a0abeed4398577f16b9c3b52c44e24a218
Scott Schlosser <scottschlosser@gmail.com>
2012-11-17 10:39:57 -0800

    Even better than commenting out... Switched the order of the jobs

--- lib/clock.rb
+++ lib/clock.rb
@@ -5,7 +5,6 @@ require 'clockwork'
 require 'delayed_job'
 include Clockwork
 
-#every(240.minutes, 'twitter.list.job') { TwitterList.all.each { |tl| Delayed::Job.enqueue tl }} 
 every(90.minutes, 'member.tweet.job') { Member.all.each { |member| Delayed::Job.enqueue member }} 
-
+every(240.minutes, 'twitter.list.job') { TwitterList.all.each { |tl| Delayed::Job.enqueue tl }}
 
