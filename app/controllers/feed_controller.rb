4c27f630a146eeada0782971bb8b341c0ebd2cd8
Scott Schlosser <SSchlosser@wetpaint.com>
2012-11-19 16:59:58 -0800

    Fixing the twittter timestamp. Correcting ordering of tweets according to twitter timestamp. Adjusting formating of twitter timestamp

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -4,10 +4,10 @@ class FeedController < ApplicationController
   	@property = params[:slug]
 		@twitter_lists = TwitterList.all
   	if @property.nil?
-  		@image_tweets = Tweet.order('created_at DESC').page params[:page]
+  		@image_tweets = Tweet.order('twitter_created_at DESC').page params[:page]
   		@property = "All"
   	else
-			@image_tweets = Tweet.order('created_at DESC').joins(:twitter_list_tweets).joins(:twitter_lists).where('twitter_lists.list_slug = ?', @property).page params[:page]
+			@image_tweets = Tweet.order('twitter_created_at DESC').joins(:twitter_list_tweets).joins(:twitter_lists).where('twitter_lists.list_slug = ?', @property).page params[:page]
 
     end
   end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,5 @@
 class Tweet < ActiveRecord::Base
-  attr_accessible :display_url, :expanded_url, :id, :user_id, :media_url, :media_url_https, :source, :text, :twitter_create_at, :twitter_id, :url
+  attr_accessible :display_url, :expanded_url, :id, :user_id, :media_url, :media_url_https, :source, :text, :twitter_create_at, :twitter_created_at, :twitter_id, :url
   has_many :twitter_lists, :through => :twitter_list_tweets
   has_many :twitter_list_tweets
 	paginates_per 49 
